f6e0f2f64acae50744ea19419d8019508af53da6
Ingit <ingitag777@gmail.com>
2019-03-18 10:43:26 +0530

    commit 3 comparasion of three no.

--- task2.cpp
+++ task2.cpp
@@ -1,15 +1,25 @@
+// comparasion of three no.s
 #include<iostream>
 using namespace std;
 int main()
 {
-	int a,b;
+	int a,b,c;
 	cout<<"Enter the value of a and b\n";    //input call
 	cin>>a>>b;
-	if(a<b)           
-		cout<<"b is greater than a";
-	if(a>b)
-		cout<<"a is greater than b";
-	else 
-		cout<<"both are equal";
+	// conditions
+	if(a<b&&a<c)           
+		cout<<"b  and c are greater than a";
+	if(a>b&&a>c)
+		cout<<"a is greater than b and c";
+	if(b>a&&b>c)
+		cout<<"b is greater than a amd c";
+	if(b<a&&b<c)
+		cout<<"b is less than a and c";
+	 if(c>a&&c>b)
+                cout<<"c is greater than a and b";
+        if(c<a&&c<b)
+                cout<<"c is less than a and b";
+	else
+		cout<<"all are equal";    // final condition
 	return 0;
 }
