cf795f4021ecacc4709ac18d16cf5cc840f9ecfc
Ingit <ingitag777@gmail.com>
2019-03-18 10:35:18 +0530

    commit 1 comparasion of two no.s

---
+++ task2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a and b\n";
+	cin>>a>>b;
+	if(a<b)
+		cout<<"b is greater than a";
+	if(a>b)
+		cout<<"a is greater than b";
+	else 
+		cout<<"both are equal";
+	return 0;
+}
